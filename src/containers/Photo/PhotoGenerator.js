f6337013f5170e900ae2eef5481b40de9c74ca4a
Min Thet Naing <me@minthetnaing.xyz>
2017-03-22 18:09:03 +0800

    Add react-konva for canvas drawing and draw some raw photo

--- src/containers/Photo/PhotoGenerator.js
+++ src/containers/Photo/PhotoGenerator.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import TextareaAutosize from 'react-textarea-autosize';
-import html2canvas from 'html2canvas';
+import {Layer, Image, Stage, Text, Rect, Group } from 'react-konva';
 
 import LoadingErrorWrapper from 'components/General/LoadingErrorWrapper';
 
@@ -9,7 +9,19 @@ class PhotoGenerator extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      image: null,
       textValue: null
+    }
+  }
+
+  componentDidMount() {
+    const image = new window.Image();
+    image.setAttribute('crossOrigin', 'anonymous');
+    image.src = 'https://images.unsplash.com/photo-1489564239502-7a532064e1c2?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&s=161f6f5fde3b165653824fb4b9e21f40';
+    image.onload = () => {
+      this.setState({
+        image: image
+      });
     }
   }
 
@@ -20,39 +32,63 @@ class PhotoGenerator extends Component {
   }
 
   generatePhoto = () => {
-    const { defaultPhoto } = this.props;
-    html2canvas(this.textImageWrapper, {
-      proxy: defaultPhoto.urls.small 
-    }).then(function(canvas) {
-      let generatedImage = canvas.toDataURL("image/png");
-      location.href = generatedImage;
-    });
+    let generatedImage = this.layer.canvas.toDataURL();
+    location.href = generatedImage;
   }
-  
+
   render() {
     const { photo, defaultPhoto } = this.props;
     const { isFetching, error } = photo;
-    const { textValue } = this.state;
+    const { textValue, image } = this.state;
+    const renderImage = image;
+    if (defaultPhoto && renderImage) {
+      renderImage.src = defaultPhoto.urls.regular;
+    }
     return (
       <div className="col-12">
-        <div className="row justify-content-md-center" style={{ marginBottom: '2em' }}>
-          <div className="col-md-6 col-sm-12">
+        <div className="row" style={{ marginBottom: '2em' }}>
+          <div className="col-md-12 col-sm-12">
             <LoadingErrorWrapper loading={isFetching} error={error}>
               {defaultPhoto && (
-                <div 
-                  className="textImageWrapper" 
-                  style={{ width: '400px' }}
-                  ref={(textImageWrapper) => { this.textImageWrapper = textImageWrapper; }}
-                >
-                  <img className="textImageImg img-responsive" src={defaultPhoto.urls.small} alt="Unsplash" />
-                  {textValue && (
-                    <span className="textImageText">{textValue}</span>
-                  )}
-                </div>
+                  <Stage width={renderImage ? renderImage.width : 400} height={renderImage ? renderImage.height : 267} ref={(stage) => { this.stage = stage; }}>
+                    <Layer ref={(layer) => { this.layer = layer; }} style={{ textAlign: 'center' }}>
+                      <Image
+                        image={renderImage}
+                      />
+                      <Group
+                        draggable
+                        dragBoundFunc={(pos) => {
+                          return {
+                            x: this.group ? this.group.getAbsolutePosition().x : pos.x,
+                            y: pos.y
+                          }
+                        }}
+                        ref={(group) => { this.group = group; }}
+                      >
+                        <Text
+                          text={textValue}
+                          fontSize={30}
+                          draggable={true}
+                          fontFamily='Calibri'
+                          fill='#fff'
+                          width={renderImage ? renderImage.width : 400}
+                          shadowColor='black'
+                          align='center'
+                          ref={(text)  => { this.text = text; }}
+                        />
+                        <Rect
+                          width={renderImage ? renderImage.width : 400}
+                          height={this.text ? this.text.getHeight() + 10 : null}
+                          fill="#000"
+                          opacity={0.4}
+                        />
+                      </Group>
+                    </Layer>
+                  </Stage>
               )}
-            </LoadingErrorWrapper> 
+            </LoadingErrorWrapper>
           </div>
-          <div className="col-md-6 col-sm-12">
+          <div className="col-md-12 col-sm-12">
             <TextareaAutosize
               minRows={1}
               className="form-control"
